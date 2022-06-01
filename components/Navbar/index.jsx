f49ae5900523fe9b1fe91ac9a5c752944886c1ca
PaoloRotondo23 <paolorotondo23@gmail.com>
2022-06-01 12:45:54 +0200

    Add navbar component

---
+++ components/Navbar/index.jsx
@@ -0,0 +1,26 @@
+import Link from 'next/link'
+import styles from './styles.module.scss'
+
+export default function Navbar() {
+    return (
+        <nav className={styles.navbar}>
+            <div>
+                <ul>
+                    <h2>TRICILY</h2>
+                        <Link href="/pages/ContactUs.jsx">
+                            <a>CONTACT US</a>
+                        </Link>
+                        <Link href="#">
+                            <a>ABOUT US</a>
+                        </Link>
+                        <Link href="#">
+                            <a>EXPERIENCES</a>
+                        </Link>
+                        <Link href="#">
+                            <a>HOME</a>
+                        </Link>
+                </ul>
+            </div>
+        </nav>
+    )
+}
