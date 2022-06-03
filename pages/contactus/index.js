3aa483068edc737600b3277f36315bfaf0b1f71a
ELENIA30 <elenia.centamore30@gmail.com>
2022-06-03 10:06:00 +0200

    feat:added mediaqueries

--- pages/contactus/index.js
+++ pages/contactus/index.js
@@ -3,21 +3,20 @@ import styles from "./style.module.scss";
 import Image from "next/image";
 export default function ContactUs() {
   return (
-  
-    <div className={styles.contact}>
-      <Image
-        src="https://i.ibb.co/xML1pGy/lapacontactus.jpg"
-        alt="lapacontactus"
-        width={2048}
-        height={1280}
-        layout="responsive"
-        className={styles.backimg}
-      ></Image>
-      <div className={styles.contact}>
-        <Contact/>
+    <div className={styles.wrapper}>
+      <div className={styles.contactImg}>
+        <img
+          src="https://i.ibb.co/xML1pGy/lapacontactus.jpg"
+          alt="lapacontactus"
+          width={2048}
+          height={1280}
+         
+          className={styles.backimg}
+        ></img>
       </div>
-      
+      <div className={styles.contact}>
+        <Contact />
+      </div>
     </div>
-    
   );
 }
