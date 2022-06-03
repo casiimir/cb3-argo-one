43f1ec9000777993eba583735783cb3e8c827f4f
Andrea Cicala <andreacicala.93@gmail.com>
2022-06-03 12:04:32 +0200

    feat: experience page added

--- pages/experiences/index.jsx
+++ pages/experiences/index.jsx
@@ -1,3 +1,9 @@
+<<<<<<< HEAD
 import Experiences from "./experiencesPage";
 
 export default Experiences;
+=======
+import Experience from "./datepicker";
+
+export default Experience;
+>>>>>>> feat: experience page added
