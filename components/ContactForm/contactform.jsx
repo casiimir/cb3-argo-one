bae3874c9679e63cfd1184965ff562619e21f693
Federica <centorbifederica@gmail.com>
2022-06-04 17:24:12 +0200

    feat: styling contact us page

--- components/ContactForm/contactform.jsx
+++ components/ContactForm/contactform.jsx
@@ -1,45 +1,40 @@
 import styles from "./styles.module.scss";
 
 export default function Contact() {
-  return (
-    <div className={styles.wrapper}>
-      <div className={styles.title}>
-          <h1> Send us a message </h1>
-        </div>
-      <form className={styles.wrapper__form}>
-        
-        <div className={styles.input}>
-          <label htmlFor="Name"></label>
-          <input
-            className={styles.wrapper__input}
-            type="text"
-            id="name"
-            placeholder="Your name..."
-          ></input>
-          </div>
-          <div className={styles.input}>
-          <label htmlFor="Email"></label>
-          <input
-            className={styles.wrapper__input}
-            type="text"
-            id="email"
-            placeholder="Your email..."
-          ></input>
-        </div>
+	return (
+		<div className={styles.wrapper}>
+			<h1 className={styles.wrapper__title}>Send us a message</h1>
+			<form className={styles.wrapper__form}>
+				<div className={styles.wrapper__inputAll}>
+					<input
+						className={styles.wrapper__input}
+						type="text"
+						id="name"
+						placeholder="Your name..."
+						required
+					></input>
+					<input
+						className={styles.wrapper__input}
+						type="text"
+						id="email"
+						placeholder="Your email..."
+						required
+					></input>
+				</div>
 
-        <br />
+				<div className={styles.wrapper__message}>
+					<label htmlFor="Message"></label>
+					<textarea
+						type="text"
+						id="message"
+						placeholder="Your message..."
+						required
+						className={styles.wrapper__textarea}
+					></textarea>
 
-        <div className={styles.message}>
-          <label htmlFor="Message"></label>
-          <textarea
-            type="text"
-            id="message"
-            placeholder="Your message..."
-          ></textarea>
-          <br />
-          <button>Send</button>
-        </div>
-      </form>
-    </div>
-  );
+					<button>Send</button>
+				</div>
+			</form>
+		</div>
+	);
 }
