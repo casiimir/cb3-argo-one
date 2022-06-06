9b8bb4dca3d27766dcd2315ac52293b4f7c64595
NickPick77 <nicopico77@gmail.com>
2022-06-07 01:05:20 +0200

    FEAT: cart logic completed w/out localstorage

--- components/Navbar/index.jsx
+++ components/Navbar/index.jsx
@@ -6,10 +6,12 @@ import MobileNavigation from "./MobileNavigation";
 
 export default function Navbar() {
   return (
-    <div className={styles.Navbar}>
-      <NavLogo />
-      <Navigation />
-      <MobileNavigation />
-    </div>
+    <>
+      <div className={styles.Navbar}>
+        <NavLogo />
+        <Navigation />
+        <MobileNavigation />
+      </div>
+    </>
   );
 }
--- pages/experiences/experiencesPage.jsx
+++ pages/experiences/experiencesPage.jsx
@@ -1,32 +1,29 @@
-import { useState, useEffect } from 'react';
-import ExpCard from '../../components/ExpCard';
-import styles from './index.module.scss'
+import { useState, useEffect } from "react";
+import ExpCard from "../../components/ExpCard";
+import styles from "./index.module.scss";
 
 const Experiences = () => {
+  const [palermo, setPalermo] = useState([]);
+  const [catania, setCatania] = useState([]);
+  const [messina, setMessina] = useState([]);
 
-    const { dataStore } = useDataContext();
+  useEffect(() => {});
 
-    const [palermo, setPalermo] = useState([]);
-    const [catania, setCatania] = useState([]);
-    const [messina, setMessina] = useState([]);
-
-    useEffect(() => {
-
-    })
-
-    return (
-        <>
-        <h2 className={styles.title}>{"Here's the best experience in Tricily!"}</h2>
-            <div className={styles.global}>
-                <div className={styles.wrapper}>
-                    <ExpCard />
-                    <ExpCard />
-                    <ExpCard />
-                    <ExpCard />
-                </div>
-            </div>
-        </>
-    )
-}
+  return (
+    <>
+      <h2 className={styles.title}>
+        {"Here's the best experience in Tricily!"}
+      </h2>
+      <div className={styles.global}>
+        <div className={styles.wrapper}>
+          <ExpCard />
+          <ExpCard />
+          <ExpCard />
+          <ExpCard />
+        </div>
+      </div>
+    </>
+  );
+};
 
 export default Experiences;
