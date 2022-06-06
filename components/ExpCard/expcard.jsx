7e6519c097738c828154342cded727c927dbdee5
Andrea Cicala <andreacicala.93@gmail.com>
2022-06-06 14:55:19 +0200

    branch fix

--- components/ExpCard/expcard.jsx
+++ components/ExpCard/expcard.jsx
@@ -3,9 +3,9 @@ import { useRef } from 'react';
 import { useDataContext } from '../../context/DataContext/dataContext';
 import styles from './index.module.scss';
 
-const ExpCard = () => {
-    const { dataStore } = useDataContext();
-    const experiences = dataStore.experiences.data;
+const ExpCard = ( props ) => {
+
+    const results = props.results;
 
     const scrl = useRef(null);
     const slide = (cardWidth = 200) => {
@@ -14,29 +14,28 @@ const ExpCard = () => {
     };
 
     return (
-        <> 
-        <div className={styles.arrow_div}>
-            <Arrows
-                dx={() => (scrl.current.scrollLeft += slide())}
-                sx={() => (scrl.current.scrollLeft += -1 * slide())}
-            />
-        </div>
-        
-        <div className={styles.wrapper} ref={scrl}>
-            {experiences && experiences.map((single, index) => (
-                <div className={styles.cards} key={index}>
-                    <img className={styles.card_img}
-                        src={single.cover_image_url}
-                        alt={single.title}
-                    />
-                    <div className={styles.text}>
-                        <h2 className={styles.card_title}>{single.title}</h2>
-                        <p className={styles.desc}>{single.description}</p>
+        <>
+            <div className={styles.arrow_div}>
+                <Arrows
+                    dx={() => (scrl.current.scrollLeft += slide())}
+                    sx={() => (scrl.current.scrollLeft += -1 * slide())}
+                />
+            </div>
+            <div className={styles.wrapper} ref={scrl}>
+                {results && results.map((single, index) => (
+                    <div className={styles.cards} key={index}>
+                        <img className={styles.card_img}
+                            src={single.cover_image_url}
+                            alt={single.title}
+                        />
+                        <div className={styles.text}>
+                            <h2 className={styles.card_title}>{single.title}</h2>
+                            <p className={styles.desc}>{single.description}</p>
+                        </div>
                     </div>
-                </div>
-            ))}
-        </div>
-    </>
+                ))}
+            </div>
+        </>
     )
 }
 
--- pages/experiences/experiencesPage.jsx
+++ pages/experiences/experiencesPage.jsx
@@ -1,15 +1,24 @@
+import { useState, useEffect } from 'react';
 import ExpCard from '../../components/ExpCard';
 import styles from './index.module.scss'
 
 const Experiences = () => {
     const { dataStore } = useDataContext();
-    const experiences = dataStore.experiences;
 
+    const [palermo, setPalermo] = useState([]);
+    const [catania, setCatania] = useState([]);
+    const [messina, setMessina] = useState([]);
+
+    useEffect(() => {
+
+    })
 
     return (
         <>
+        <h2 className={styles.title}>{"Here's the best experience in Tricily!"}</h2>
             <div className={styles.global}>
                 <div className={styles.wrapper}>
+                    <ExpCard />
                     <ExpCard />
                     <ExpCard />
                     <ExpCard />
