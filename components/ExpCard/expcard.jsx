bc97901f48e69acee14152893a7168e4a366017c
Andrea Cicala <andreacicala.93@gmail.com>
2022-06-06 19:10:24 +0200

    feat: added detail page structure

--- components/ExpCard/expcard.jsx
+++ components/ExpCard/expcard.jsx
@@ -2,8 +2,15 @@ import Arrows from '../Arrows';
 import { useRef } from 'react';
 import { useDataContext } from '../../context/DataContext/dataContext';
 import styles from './index.module.scss';
+import Link from 'next/link';
+import { useModalContext } from '../../context/ModalContext/modalContext';
 
 const ExpCard = ( {result} ) => {
+    
+    const { setOpenModal } = useModalContext();
+    const handleClickOnCard = (cardData) => {
+        setOpenModal(cardData, true);
+      };
 
     const data = result;
 
@@ -23,11 +30,14 @@ const ExpCard = ( {result} ) => {
             </div>
             <div className={styles.wrapper} ref={scrl}>
                 {data && data.map((single, index) => (
-                    <div className={styles.cards} key={index}>
+                    
+                    <div className={styles.cards} key={index} onClick={() => handleClickOnCard(single)}>
+                        <Link href={`/detail/${single.slug_id}`}><a>
                         <img className={styles.card_img}
                             src={single.cover_image_url}
                             alt={single.title}
                         />
+                        </a></Link>
                         <div className={styles.text}>
                             <h2 className={styles.card_title}>{single.title}</h2>
                             <p className={styles.desc}>{single.description}</p>
