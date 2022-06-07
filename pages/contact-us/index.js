cd22faa9ad870168312b61c67f1d0331b7494fab
ELENIA30 <elenia.centamore30@gmail.com>
2022-06-07 17:13:28 +0200

    contactus/finish

--- pages/contact-us/index.js
+++ pages/contact-us/index.js
@@ -1,9 +1,11 @@
-import Contact from "../../components/ContactForm";
+// import Contact from "../../components/ContactForm";
 import styles from "./style.module.scss";
 import Image from "next/image";
+
 export default function ContactUs() {
   return (
     <div className={styles.wrapper}>
+      <h1 className={styles.wrapper__title}>Contact us</h1>
       <div className={styles.contactImg}>
         <Image
           src="https://i.ibb.co/xML1pGy/lapacontactus.jpg"
@@ -11,13 +13,39 @@ export default function ContactUs() {
           width={2400}
           height={1598}
           layout="responsive"
-     
-         
+          objectFit="cover"
           className={styles.backimg}
         ></Image>
-      </div>
-      <div className={styles.contact}>
-        <Contact />
+
+        <div className={styles.form}>
+          <h2> Send us a message </h2>
+          <form className={styles.lapa}>
+            <div className={styles.input}>
+              <input
+                className={styles.wrapper__input}
+                type="text"
+                id="name"
+                placeholder="Your name..."
+              ></input>
+              <input
+                className={styles.wrapper__input}
+                type="text"
+                id="email"
+                placeholder="Your email..."
+              ></input>
+            </div>
+
+            <div className={styles.message}>
+              <textarea
+                className={styles.textarea}
+                type="text"
+                id="message"
+                placeholder="Your message..."
+              ></textarea>
+              <button className={styles.button}>Send</button>
+            </div>
+          </form>
+        </div>
       </div>
     </div>
   );
