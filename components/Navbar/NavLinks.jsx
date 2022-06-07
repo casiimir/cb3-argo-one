d54101abb1846659682501e4524eb21dffd935af
PaoloRotondo23 <101108407+PaoloRotondo23@users.noreply.github.com>
2022-06-07 19:55:30 +0200

    Feature/navbar (#69)
    * Feat: Add Navbar
    
    * feat/created navbar component
    
    * feat/added scrollup feature
    
    * fix
    
    * feat: Scroll Up Button
    
    Co-authored-by: Andrea Cicala <101107671+AndreaCicala@users.noreply.github.com>

--- components/Navbar/NavLinks.jsx
+++ components/Navbar/NavLinks.jsx
@@ -1,11 +1,39 @@
-import Link from "next/link";
-import { useDataContext } from "../../context/DataContext/dataContext";
-import { FaShoppingCart } from "react-icons/fa";
-import { cities } from "../../utils/data";
-import styles from "./styles.module.scss";
+import Link from 'next/link';
+import styles from './styles.module.scss';
+import {FaShoppingCart} from 'react-icons/fa';
+import {RiUser3Fill} from 'react-icons/ri';
 
 const NavLinks = (props) => {
-  const { updateExperiencesData } = useDataContext();
+    return (
+        <nav>
+            <ul className={props.open?styles.MenuOpener:null}>
+                <Link href="/" onClick={() => props.isMobile && props.closeMobileMenu()}>
+                    <a>HOME</a>
+                </Link>
+                <Link href="/experiences" onClick={() => props.isMobile && props.closeMobileMenu()}>
+                    <a>EXPERIENCES</a>
+                </Link>
+                <Link href="/about" onClick={() => props.isMobile && props.closeMobileMenu()}>
+                    <a>ABOUT</a>
+                </Link>
+                <Link href="/contact-us" onClick={() => props.isMobile && props.closeMobileMenu()}>
+                    <a>CONTACTS</a>
+                </Link>
+                <Link href="/" onClick={() => props.isMobile && props.closeMobileMenu()}>
+                    <a>
+                    <RiUser3Fill className={styles.NavbarIcons} />
+                    </a>
+                </Link>
+                <Link href="/" onClick={() => props.isMobile && props.closeMobileMenu()}>
+                    <a>
+                        <FaShoppingCart className={styles.NavbarIcons} />
+                    </a>
+                </Link>
+            </ul>
+        </nav>
+    );
+}    
+
 
   const handleClickOnExperience = () => {
     const cityId = cities.map((city) => city.id);
