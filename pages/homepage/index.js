e6caa08fa5c8dd9e3636c91fba580476c0b7f3bd
NickPick77 <nicopico77@gmail.com>
2022-06-07 14:05:50 +0200

    FEAT: static Hero for mobile & dynamic for desktop, changend img

--- pages/homepage/index.js
+++ pages/homepage/index.js
@@ -2,18 +2,15 @@ import { useState, useEffect } from "react";
 import { useWindowSize } from "../../utils/utils";
 import Image from "next/image";
 
-import DynamicParagraph from "../../components/DynamicParagraph/dynamicParagraph";
+import Hero from "../../components/Hero/hero";
 
 import styles from "./styles.module.scss";
-import Hero from "../../components/Hero";
 
 export default function Home() {
   return (
     <div className={styles.homepage}>
       <Hero />
-      <div className={styles.homepage__desc}>
-        <DynamicParagraph />
-      </div>
+      <div className={styles.homepage__desc}></div>
     </div>
   );
 }
