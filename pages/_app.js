f69e77fb67b858d8ebb646e9aaac6d582829e42f
NickPick77 <nicopico77@gmail.com>
2022-06-08 22:29:20 +0200

    FEAT: cart stylized

--- pages/_app.js
+++ pages/_app.js
@@ -4,12 +4,6 @@ import DefaultLayout from "../components/Layout";
 import { config } from "@fortawesome/fontawesome-svg-core";
 import "@fortawesome/fontawesome-svg-core/styles.css";
 import "../styles/globals.css";
-import { config } from "@fortawesome/fontawesome-svg-core";
-import "@fortawesome/fontawesome-svg-core/styles.css";
-
-config.autoAddCss = false;
-
-config.autoAddCss = false;
 
 config.autoAddCss = false;
 
