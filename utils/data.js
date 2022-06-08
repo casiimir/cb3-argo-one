abe2573ab7778fdee74c9387c22fadad75a89311
Federica <centorbifederica@gmail.com>
2022-06-08 14:57:12 +0200

    feat: homepage completed

--- utils/data.js
+++ utils/data.js
@@ -1,32 +1,32 @@
 export const cities = [
-  {
-    name: "Palermo",
-    id: 24,
-  },
-  {
-    name: "Catania",
-    id: 15,
-  },
-  {
-    name: "Messina",
-    id: 572,
-  },
-  {
-    name: "Agrigento",
-    id: 265,
-  },
-  {
-    name: "Trapani",
-    id: 587,
-  },
-  {
-    name: "Ragusa",
-    id: 147,
-  },
-  {
-    name: "Siracusa",
-    id: 147,
-  },
+	{
+		name: "Palermo",
+		id: 24,
+	},
+	{
+		name: "Catania",
+		id: 15,
+	},
+	{
+		name: "Messina",
+		id: 572,
+	},
+	{
+		name: "Agrigento",
+		id: 265,
+	},
+	{
+		name: "Trapani",
+		id: 587,
+	},
+	{
+		name: "Ragusa",
+		id: 147,
+	},
+	{
+		name: "Siracusa",
+		id: 147,
+	},
 ];
 
 export const mainCities = [
@@ -35,45 +35,45 @@ export const mainCities = [
 ];
 
 export const categories = [
-  {
-    name: "Tour e Attrazioni",
-    code: "sightseeing",
-  },
-  {
-    name: "Arte e musei",
-    code: "arts-culture",
-  },
-  {
-    name: "Food e Wine",
-    code: "food-wine",
-  },
-  {
-    name: "Sport e Avventura",
-    code: "adventure",
-  },
+	{
+		name: "Tour e Attrazioni",
+		code: "sightseeing",
+	},
+	{
+		name: "Arte e musei",
+		code: "arts-culture",
+	},
+	{
+		name: "Food e Wine",
+		code: "food-wine",
+	},
+	{
+		name: "Sport e Avventura",
+		code: "adventure",
+	},
 ];
 
 export const slideData = {
-  textData: [
-    "our food...",
-    "our nature...",
-    "our traditions...",
-    "our history...",
-    "our beaches",
-  ],
-  imgUrl: [
-    "https://i.ibb.co/S7T1Dgz/2950545139-49f3999480-o.jpg",
-    "https://i.ibb.co/j5TndtQ/15904296544-cc7b2f91f1-o.jpg",
-    "https://i.ibb.co/JymF8FJ/50953309487-7eb2dcdf4e-o.jpg",
-    "https://i.ibb.co/cYg3d94/10493735375-a69c0ab83d-o.jpg",
-    "https://i.ibb.co/hKkLvz1/33105324400-1c1a6921c0-o.jpg",
-  ],
-  mobileImgUrl: [
-    //"https://i.ibb.co/Ptp5MS0/agrigento6.jpg",
-    //"https://i.ibb.co/8rV78xg/etna.jpg",
-    //"https://i.ibb.co/RvSHhR4/3.jpg",
-    "https://i.ibb.co/jhLmN9K/limoni.jpg",
-    //"https://i.ibb.co/mJ6c6Sq/granita-Home2.jpg",
-    //"https://i.ibb.co/hFkrYbq/vilija-valeisaite-x-TZpip-USEj8-unsplash.jpg",
-  ],
+	textData: [
+		"our food",
+		"our nature",
+		"our traditions",
+		"our history",
+		"our beaches",
+	],
+	imgUrl: [
+		"https://i.ibb.co/S7T1Dgz/2950545139-49f3999480-o.jpg",
+		"https://i.ibb.co/j5TndtQ/15904296544-cc7b2f91f1-o.jpg",
+		"https://i.ibb.co/JymF8FJ/50953309487-7eb2dcdf4e-o.jpg",
+		"https://i.ibb.co/cYg3d94/10493735375-a69c0ab83d-o.jpg",
+		"https://i.ibb.co/hKkLvz1/33105324400-1c1a6921c0-o.jpg",
+	],
+	mobileImgUrl: [
+		//"https://i.ibb.co/Ptp5MS0/agrigento6.jpg",
+		//"https://i.ibb.co/8rV78xg/etna.jpg",
+		//"https://i.ibb.co/RvSHhR4/3.jpg",
+		"https://i.ibb.co/jhLmN9K/limoni.jpg",
+		//"https://i.ibb.co/mJ6c6Sq/granita-Home2.jpg",
+		//"https://i.ibb.co/hFkrYbq/vilija-valeisaite-x-TZpip-USEj8-unsplash.jpg",
+	],
 };
