887583652801dcce0d0c17a2a66f13cd189a4ae1
PaoloRotondo23 <101108407+PaoloRotondo23@users.noreply.github.com>
2022-06-11 13:05:47 +0200

    Feature/navbar (#91)
    * Feat: Add Navbar
    
    * feat/created navbar component
    
    * feat/added scrollup feature
    
    * feat: Scroll Up Button
    
    * feat: Navbar Finished

--- components/Navbar/index.jsx
+++ components/Navbar/index.jsx
@@ -1,8 +1,7 @@
 import NavLogo from "./NavLogo";
-import styles from "./styles.module.scss";
-import React, { useState } from "react";
-import Navigation from "./Navigation";
-import MobileNavigation from "./MobileNavigation";
+import styles from './styles.module.scss';
+import Navigation from './Navigation';
+import MobileNavigation from './MobileNavigation';
 
 export default function Navbar() {
   return (
